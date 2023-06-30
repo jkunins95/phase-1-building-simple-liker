5d24dd287eb544db12e202b1c870b2d9b4ec80f8
+ <jkunins95@gmail.com>
2023-06-30 19:23:52 -0400

    completed lab

--- main.js
+++ main.js
@@ -4,12 +4,40 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.getElementById("modal");
+modal.classList.add("hidden");
+
+const likeButtons = document.querySelectorAll(".like-glyph");
+likeButtons.forEach(handleLike);
+
+function handleLike(item) {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+    if(e.target.innerText === EMPTY_HEART){
+      mimicServerCall()
+      .then(() => {
+        e.target.innerText = FULL_HEART;
+        e.target.classList.add("activated-heart");
+      })
+      .catch((error) => {
+        document.getElementById("modal").classList.remove("hidden");
+        document.getElementById("modal-message").innerText = error.message;
+        setTimeout(() => document.getElementById("modal").classList.add("hidden"), 3000);
+      })      
+    }
+    else{
+      e.target.innerText = EMPTY_HEART;
+      e.target.classList.remove("activated-heart");
+    }   
+  });
+}
 
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
+
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
